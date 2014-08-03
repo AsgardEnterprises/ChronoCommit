3eb208f305fa1a7f21bfc061a054c4938fde20e4
Marcel Cutts <me@marcelcutts.com>
2014-08-03 18:36:19 +0100

    Added application

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function() {
+	'use strict';
+
+	// Declare app level module which depends on filters, and services
+	angular.module('chronoCommit', [
+		'chronoCommit.filters',
+		'chronoCommit.services',
+		'chronoCommit.directives',
+		'chronoCommit.controllers'
+	]);
+})();
